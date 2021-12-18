eb46eccccf240da805863ba7e445939350314ce6
mrbaj <bajrang04.rajpurohit@gmail.com>
2021-12-18 18:01:28 +0530

    3.4 shared module singleton loading

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -13,7 +13,11 @@ module.exports = {
 			exposes: {
 				"./showCart": "./src/index",
 			},
-			shared: ["faker"],
+			shared: {
+				faker: {
+					singleton: true,
+				},
+			},
 		}),
 		new HtmlWebpackPlugin({
 			template: "./public/index.html",
