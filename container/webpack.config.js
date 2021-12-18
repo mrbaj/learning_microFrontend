38c0d28db63cb94f681416bc6f7efadd2209d5c9
mrbaj <bajrang04.rajpurohit@gmail.com>
2021-12-18 12:00:43 +0530

    2.1 adding module federation plugin in webpack of main project (container)

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFedetationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
 	mode: "development",
@@ -6,6 +7,12 @@ module.exports = {
 		port: 8080,
 	},
 	plugins: [
+		new ModuleFedetationPlugin({
+			name: "container",
+			remotes: {
+				products: "products@http://localhost:8081/remoteEntry.js",
+			},
+		}),
 		new HtmlWebpackPlugin({
 			template: "./public/index.html",
 		}),
