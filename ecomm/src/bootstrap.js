819675da98e564f4f6d36c085b847021a60eb356
mrbaj <bajrang04.rajpurohit@gmail.com>
2021-12-18 18:35:35 +0530

    3.5.1 Refactoring Products(ecomm) for standalone app

--- ecomm/src/bootstrap.js
+++ ecomm/src/bootstrap.js
@@ -10,3 +10,11 @@ export function mount(el) {
 
 	el.innerHTML = products;
 }
+
+if (process.env.NODE_ENV === "development") {
+	const el = document.querySelector("#dev-products");
+
+	if (el) {
+		mount(el);
+	}
+}
