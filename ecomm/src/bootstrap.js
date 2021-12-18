4c6b5f8babab701d76b199e9dc5f65b81ebf96a1
mrbaj <bajrang04.rajpurohit@gmail.com>
2021-12-18 18:29:57 +0530

    3.5 Refactoring Products(ecomm)

--- ecomm/src/bootstrap.js
+++ ecomm/src/bootstrap.js
@@ -1,10 +1,12 @@
 import faker from "faker";
 
-let products = "";
+export function mount(el) {
+	let products = "";
 
-for (let i = 0; i < 5; i++) {
-	const name = faker.commerce.productName();
-	products += `<div>${name}</div>`;
+	for (let i = 0; i < 5; i++) {
+		const name = faker.commerce.productName();
+		products += `<div>${name}</div>`;
+	}
+
+	el.innerHTML = products;
 }
-
-document.querySelector("#dev-products").innerHTML = products;
